29945b1ff2334cb0dcc9746b5386cd9377300ee0
Lawrence <791785648@qq.com>
2017-08-03 10:24:45 +0800

    Initial commit

---
+++ Example/WJBaseComponent/WJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WJAppDelegate.h
+//  WJBaseComponent
+//
+//  Created by CoderLawrence on 08/03/2017.
+//  Copyright (c) 2017 CoderLawrence. All rights reserved.
+//
+
+@import UIKit;
+
+@interface WJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
